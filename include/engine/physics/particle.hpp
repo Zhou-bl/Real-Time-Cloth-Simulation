171e84b1875e1e0999401a532062b466735e5bff
Ubuntu64 <zhoubinglin2002@163.com>
2024-05-31 14:39:36 -0700

    init

---
+++ include/engine/physics/particle.hpp
@@ -0,0 +1,52 @@
+#pragma once
+#include <SFML/System/Vector2.hpp>
+#include "../common/index_vector.hpp"
+
+
+struct Particle
+{
+    civ::ID      id            = 0;
+    float        mass          = 1.0f;
+    bool         moving        = true;
+    sf::Vector2f position;
+    sf::Vector2f position_old;
+    sf::Vector2f velocity;
+    sf::Vector2f forces;
+
+    Particle() = default;
+
+    explicit
+    Particle(sf::Vector2f pos)
+        : position(pos)
+        , position_old(pos)
+    {}
+
+    void update(float dt)
+    {
+        if (!moving) return;
+        position_old = position;
+        velocity += (forces / mass) * dt;
+        position += velocity * dt;
+    }
+
+    void updateDerivatives(float dt)
+    {
+        velocity = (position - position_old) / dt;
+        forces = {};
+    }
+
+    void updateExternalCollision(){
+        if (position.x > 1760) {
+            position.x = 1760;
+            velocity.x = -0.5 * velocity.x;
+        }
+    }
+
+    void move(sf::Vector2f v)
+    {
+        if (!moving) return;
+        position += v;
+    }
+};
+
+using ParticleRef = civ::Ref<Particle>;
